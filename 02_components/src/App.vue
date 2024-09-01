ea6f141c93b46e75ffc1d923f2cb4aad13390c00
olegomio <geiger.ral@gmail.com>
2024-09-01 22:01:15 +0200

    test sfc + import

--- 02_components/src/App.vue
+++ 02_components/src/App.vue
@@ -1,16 +1,21 @@
 <template>
 	<div id="app">
 		<TodoList />
-	</div>
+		<SingleFileComponent />
+	</div>	
 </template>
 
 <script>
 	import TodoList from './components/TodoList.vue';
+	import SingleFileComponent from './components/Single-File-Components.vue';
 
 	export default {
 		name: 'App',
 		components: {
-			TodoList
+			TodoList,
+			SingleFileComponent
 		}
 	};
+
+
 </script>
---
+++ 02_components/src/components/Single-File-Components.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import { ref } from 'vue'
+  const greeting = ref('geschafft du spasst')
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
