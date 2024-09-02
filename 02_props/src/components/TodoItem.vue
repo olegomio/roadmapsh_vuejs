ec7f3d12263d75dc24ae3f4b1a7459365139a2de
olegomio <geiger.ral@gmail.com>
2024-09-02 11:23:25 +0200

    init props excercise

---
+++ 02_props/src/components/TodoItem.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps(['item'])
+</script>
+
+<template>
+  <li>
+    <input type="checkbox">
+    {{ item }}
+  </li> 
+</template>
---
+++ 02_props/src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+import TodoItem from './TodoItem.vue'
+
+</script>
+
+<template>
+  <ul>
+    <TodoItem 
+      v-for="item in items"
+      :key="item.id"
+      :title="item.title"
+    />
+  </ul>
+</template>
