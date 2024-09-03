5a8b0731b22ef73aff61127af83520b81d027b1c
olegomio <geiger.ral@gmail.com>
2024-09-03 17:45:34 +0200

    init components

---
+++ 02_v-model/src/components/UserDisplay.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import UserForm from './UserForm.vue'
+
+</script>
+
+<template>
+
+  <h1>Display</h1>
+  <div>
+    <span>Name</span><br />
+    <span>{{ upName }}</span>
+  </div>
+  <div>
+    <span>Alter</span><br />
+    <span>{{ upAge }}</span>
+  </div>
+
+</template>
---
+++ 02_v-model/src/components/UserForm.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const upName = defineModel()
+  const upAge = defineModel()
+</script>
+
+<template>
+
+  <form>
+    <label>Name</label>
+    <input type="text" v-model="upName" /> 
+    
+    <label>Alter</label>
+    <input type="text" v-model="upAge" />
+  </form>
+
+</template>
